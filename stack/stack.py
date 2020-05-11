fe5e5ce9b4470a870106e26e5bde45ad95544121
Trevor Martin <trevor.j.martin@gmail.com>
2020-05-11 16:42:07 -0400

    stack class with linked-list-storage

--- stack/stack.py
+++ stack/stack.py
@@ -12,6 +12,56 @@ return elements in Last In First Out order.
 """
 
 
+class Node:
+    def __init__(self, value=None, next_node=None):
+        # the value at this linked list node
+        self.value = value
+        # reference to the next node in the list
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        # set this node's next_node reference to the passed in node
+        self.next_node = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, value):
+        """
+        add value to tail
+        """
+        new_node = Node(value)
+        # empty list
+        n = self.head
+        if not n:
+            self.head = new_node
+            return
+        # non-empty list
+        while n.get_next():
+            n = n.get_next()
+        n.set_next(new_node)
+
+    def remove(self):
+        """
+        remove value from head
+        """
+        # empty list
+        if not self.head:
+            return None
+        n = self.head.get_value()
+        # non-empty list
+        self.head = self.head.get_next()
+        return n
+
+
 class Stack1:
     def __init__(self):
         self.size = 0
@@ -32,19 +82,41 @@ class Stack1:
         return None
 
 
-# class Stack2:
-#     def __init__(self):
-#         self.size = 0
-#         # self.storage = linked list
+class Stack2:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
 
-#     def __len__(self):
-#         pass
+    def __len__(self):
+        return self.size
 
-#     def push(self, value):
-#         pass
+    def push(self, value):
+        if value:
+            self.storage.add(value)
+            self.size += 1
 
-#     def pop(self):
-#         pass
+    def pop(self):
+        newStore = LinkedList()  # crate a new storage container
+        idx = 1  # start pseudo-index at 1
+        while idx < (len(self)):
+            # add removed elements to newStore
+            newStore.add(self.storage.remove())
+            idx += 1  # increase pseudo-index
+        # newStore includes everything except the last element.
+        last_element = self.storage.remove()  # last element from storage
+        self.storage = newStore  # switch to the new linked list we created
+        if last_element:
+            self.size -= 1  # decrease element count
+        return last_element  # return the missing element
 
 
 Stack = Stack1
+
+if __name__ == "__main__":
+    soda = Stack2()
+    soda.push('cola')
+    soda.push('beer of route')
+    soda.push('ale of ginger')
+    print(soda.pop())
+    print(soda.pop())
+    print(soda.pop())
