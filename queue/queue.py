16fcff8372d69d3a170ac1eb15fa516a0a1aec51
Trevor Martin <trevor.j.martin@gmail.com>
2020-05-11 18:31:18 -0400

    Queue class with linked-list as underlying storage structure

--- queue/queue.py
+++ queue/queue.py
@@ -24,32 +24,96 @@ class Queue1:
         return self.size
 
     def enqueue(self, value):
+        """
+        add to head
+        """
         if value:
             self.storage.insert(0, value)
             self.size += 1
 
     def dequeue(self):
+        """
+        remove from tail
+        """
         if self.size > 0:
             self.size -= 1
             return self.storage.pop()
 
 
+class Node:
+    def __init__(self, value=None, next_node=None):
+        # the value at this linked list node
+        self.value = value
+        # reference to the next node in the list
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        # set this node's next_node reference to the passed in node
+        self.next_node = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, value):
+        """
+        add value to tail
+        """
+        new_node = Node(value)
+        # empty list
+        n = self.head
+        if not n:
+            self.head = new_node
+            return
+        # non-empty list
+        while n.get_next():
+            n = n.get_next()
+        n.set_next(new_node)
+
+    def remove(self):
+        """
+        remove value from head
+        """
+        # empty list
+        if not self.head:
+            return None
+        n = self.head.get_value()
+        # non-empty list
+        self.head = self.head.get_next()
+        return n
+
+
 class Queue2:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        """
+        add to back
+        """
+        if value is not None:
+            self.storage.add(value)
+            self.size += 1
 
     def dequeue(self):
-        pass
+        if self.size > 0:
+            ret = self.storage.remove()
+            self.size -= 1
+            return ret
 
 
-Queue = Queue1
+Queue = Queue2
 
 if __name__ == "__main__":
     q = Queue()
